247a98c7f625190cade7c3f29d87003921932cfb
alnig441 <alnig441@gmail.com>
2022-04-27 14:22:46 -0400

    initial commit - scrambler utility

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { onMount } from "svelte";
+	import { UTIL } from "./app_modules/UTIL.js";
+
+	let counter = 1;
+	const URL = `https://api.hatchways.io/assessment/sentences/${counter}`;
+
+	let words;
+
+	onMount(async function() {
+		words = await UTIL.getSentence(URL);
+	})
+
+</script>
+
+<main>
+	<div id="scrambled-word">{ words }</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/app_modules/UTIL.js
@@ -0,0 +1,45 @@
+export const UTIL = {
+
+  getSentence: async function(url) {
+    const RESPONSE = await fetch(url);
+    const SENTENCE = await RESPONSE.json();
+    const WORDS = SENTENCE.data.sentence.split(' ');
+    return UTIL.wordScrambler(WORDS);
+  },
+
+  wordScrambler: function(words) {
+    const LENGTH = words.length;
+    let sentence = '';
+
+    for(let i = 0 ; i < LENGTH ; i ++) {
+      if (words[i].length > 3) {
+        sentence += scramble(words[i]);
+      } else {
+        sentence += words[i];
+      }
+      if(i <= (LENGTH -1)) {
+        sentence += ' ';
+      }
+    }
+
+    return sentence;
+
+    function scramble(word){
+      const WORD = word.split('');
+      const BEGIN = WORD.shift();
+      const END = WORD.pop();
+
+      let scrambled = BEGIN;
+
+      let reducedWord = WORD;
+
+      for(let i = 0 ; i < reducedWord.length ; ) {
+        let targetIndex = Math.round(Math.random() * (reducedWord.length - 1));
+        scrambled += reducedWord.splice(targetIndex, 1);
+      }
+
+      scrambled += END;
+      return scrambled ;
+    }
+  }
+}
