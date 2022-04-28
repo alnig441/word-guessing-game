dfd0a381712819fb3162c7d948cf310366bab136
alnig441 <alnig441@gmail.com>
2022-04-28 13:10:03 -0400

    basic grid view template created

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,8 +1,19 @@
 <script>
-  import words from './Sentence.svelte';
+  import { originalSentenceAsPromise } from "../stores.js";
+  let sentence;
+
+  originalSentenceAsPromise.subscribe(value => sentence = value)
+
 </script>
 
-<div > </div>
-
+{#each sentence as word, i}
+  {#each word as letter, j}
+  <input type="text" value="{letter}">
+  {#if j === (word.length - 1) && i < (sentence.length - 1) }
+  <input type="text" value=' '>
+  {/if}
+  {/each}
+{/each}
+<button>Next</button>
 <style>
 </style>
