c0a7618557eb416f13f0ea1b3c7c74f0cd02794e
alnig441 <alnig441@gmail.com>
2022-04-29 09:44:08 -0400

    grid style complete

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -8,9 +8,10 @@
   let hide='hide' ;
   let correctAnswers = 0;
 
-  originalSentenceAsPromise.subscribe(value => sentence = value)
+  originalSentenceAsPromise.subscribe(value => {
+    sentence = value;
+   })
   scoreAsPromise.subscribe(value => score = value)
-
 
   async function getNextSentence() {
     hide = 'hide';
@@ -28,14 +29,15 @@
   }
 
   function checkLetter(e) {
-    this.removeAttribute('disabled');
+    // this.removeAttribute('disabled');
     const LENGTH = sentence.toString().length;
     let value = this.attributes['data-value'].value;
 
     if(e.key.toLowerCase() === value.toLowerCase()){
       correctAnswers ++;
       this.setAttribute('disabled', true);
-      this.setAttribute('style', "background-color:#4caf50")
+      // this.setAttribute('style', "background-color:#4caf50")
+      // this.setAttribute("class", "disabled")
     }
     if (correctAnswers === LENGTH) {
       hide ='';
@@ -45,29 +47,54 @@
   }
 
 </script>
-
-{#each sentence as word, i}
-  {#each word as letter, j}
-  <input class="letter" type="text" value="" style="" data-value="{letter}" on:keyup="{checkLetter}">
-  {#if j === (word.length - 1) && i < (sentence.length - 1) }
-  <input class="space" type="text" value="" style="" data-value=" " on:keyup="{checkLetter}">
-  {/if}
+<div id="grid">
+  {#each sentence as word, i}
+    <div class="flex-container">
+    {#each word as letter, j}
+      <div class="flex-item">
+        <input class="letter" type="text" style="" value="" data-value="{letter}" on:keyup="{checkLetter}" maxlength="1" autocomplete="off">
+      </div>
+    {#if j === (word.length - 1) && i < (sentence.length - 1) }
+      <div class="flex-item">
+        <input class="space" type="text" style="" value="" data-value=" " on:keyup="{checkLetter}" maxlength="1" autocomplete="off">
+      </div>
+    {/if}
+    {/each}
+    </div>
   {/each}
-{/each}
-<button class="{hide}" on:click="{getNextSentence}">Next</button>
+  <input type="submit" class="{hide}" on:click="{getNextSentence}" value="Next">
+</div>
 
 <style>
+  #grid {
+    padding: 5px;
+  }
+
+  .flex-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .flex-item {
+    flex-basis: auto;
+    width: 100%;
+    margin: 5px;
+  }
+
   .hide {
     display: none;
   }
 
-  button {
+  input[type="submit"] {
     color: white;
     background-color: #388e3c;
   }
 
   input {
+    width: 100%;
+    text-align: center;
     color: white;
+    margin: 0px;
   }
 
   input.space {
@@ -78,5 +105,9 @@
     background-color:  #e1e1e1;
   }
 
+  input:focus {
+    outline: none;
+    color: black;
+  }
 
 </style>
