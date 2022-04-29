0a1d4effc8b2682002116fa47683f41940eca136
alnig441 <alnig441@gmail.com>
2022-04-29 06:39:46 -0400

    styles moved to global.css

--- src/App.svelte
+++ src/App.svelte
@@ -13,20 +13,6 @@
 </main>
 
 <style>
-	main {
-		font-family: Open Sans;
-		background-color:  #e1e1e1;
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	.container {
-		/* width: 250px; */
-		color: teal;
-		margin: auto;
-	}
 
 	@media (min-width: 640px) {
 		main {
