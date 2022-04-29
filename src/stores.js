c018d97300f78e31fcb5f879c211bc4d87317fa3
alnig441 <alnig441@gmail.com>
2022-04-29 17:30:38 -0400

    promises adjusted

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const originalSentenceAsPromise = writable([]),
-              scrambledSentenceAsPromise = writable([]),
+export const  originalSentenceAsPromise = writable(''),
+              scrambledSentenceAsPromise = writable(''),
               scoreAsPromise = writable(0),
               counterAsPromise = writable(0)
