ad6d9e7b75816f3a0393e77d5066bd0b5ac611fd
alnig441 <alnig441@gmail.com>
2022-04-29 17:31:47 -0400

    win view implemented

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,22 @@
 	import Instructions from "./components/Instructions.svelte";
 	import Score from "./components/Score.svelte";
 	import Grid from "./components/Grid.svelte";
+	import {scoreAsPromise} from "./stores.js";
+
+	let score;
+	scoreAsPromise.subscribe(value => score = value);
+
 </script>
 
 <main>
+{#if score === 10 }
+		<h1> You win! </h1>
+{:else}
 	<Sentence/>
 	<Instructions/>
 	<Score/>
 	<Grid/>
+{/if}
 </main>
 
 <style>
