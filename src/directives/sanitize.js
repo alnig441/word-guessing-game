3c2c9dbe6fb44cc1bdeebba2f6da0d13f0058083
alnig441 <alnig441@gmail.com>
2022-05-02 09:38:27 -0400

    space added to regexp

--- src/directives/sanitize.js
+++ src/directives/sanitize.js
@@ -2,10 +2,10 @@ export default function sanitize(e) {
   e.preventDefault();
 
   if(this.value.length === 1 && typeof this.value === 'string') {
-    if(this.value.match(/[a-zA-Z0-9]+/g)) {
+    if(this.value.match(/[a-zA-Z0-9 ]+/g)) {
       e.target.value = this.value;
     } else {
-      e.target.value = "";
+      e.target.value = "*";
     }
   }
 }
