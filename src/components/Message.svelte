71bb8a1218150c5495bae57cf358af5ddffca200
alnig441 <alnig441@gmail.com>
2022-05-04 15:56:55 -0400

    message component added

---
+++ src/components/Message.svelte
@@ -0,0 +1,22 @@
+<script>
+export let message, type
+</script>
+
+<div>
+  <h1 class={type}>{message}</h1>
+</div>
+
+<style>
+  h1 {
+    border-radius: 5px;
+    margin: 10px;
+  }
+  .success {
+    background-color: #66bb6a;
+  }
+  .error {
+    background-color: #f44336;
+  }
+
+
+</style>
--- src/components/Sentence.svelte
+++ src/components/Sentence.svelte
@@ -2,27 +2,38 @@
 import { onMount } from "svelte";
 import { API } from "../app_modules/api";
 import { scrambledSentenceAsPromise } from "../stores.js"
+import Message from "./Message.svelte"
 
 let words;
+let message;
+let type;
 
 onMount(function() {
-  words = API.get(1);
+  words = API.get(11);
 })
 
 scrambledSentenceAsPromise.subscribe(assignWords);
 
 function assignWords(value) {
-  words = value;
+  if(typeof value === 'string') {
+    words = value;
+  }
+  else {
+    message = value.message;
+    type = 'error';
+  }
 }
 
 </script>
-
+{#if message}
+<Message {message} {type}/>
+{:else}
 <div id="sentence">
   <div id="scrambled-word">
   <h1>{ words }</h1>
   </div>
 </div>
-
+{/if}
 <style>
   h1 {
     color: steelblue;
