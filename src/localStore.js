3a2ad8cefd8bc1f368fd7f54ee675fcd73a50a1d
alnig441 <alnig441@gmail.com>
2022-05-16 12:29:28 -0400

    refactoring stores to use localStorage

---
+++ src/localStore.js
@@ -0,0 +1,22 @@
+import { writable } from 'svelte/store';
+
+export const localStore = (key, value) => {
+
+  const exists = localStorage.getItem(key);
+
+  if(!exists) {
+    localStorage.setItem(key, value);
+  }
+
+
+
+  const {subscribe, set, update} = writable(exists);
+
+  return {
+    subscribe,
+    set: (value) => {
+      localStorage.setItem(key, value)
+    },
+    update
+  };
+}
--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,12 @@
 import { writable } from 'svelte/store';
+import { localStore } from './localStore';
 
 export const  correctSentenceAsPromise = writable(''),
               scrambledSentenceAsPromise = writable(''),
               scoreAsPromise = writable(0)
+
+export const  originalSentence = localStore('originalSentence'),
+              scrambledSentence = localStore('scrambledSentence'),
+              appScore = localStore('appScore')
+
+appScore.set(0)
