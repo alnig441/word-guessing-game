bed6796ffea064a0078476fb06745e7b3f61bec8
alnig441 <alnig441@gmail.com>
2022-06-02 20:28:43 -0400

    init method refactored to reflect values in localStorage

--- src/app_modules/initialize.js
+++ src/app_modules/initialize.js
@@ -1,31 +1,18 @@
-import { scoreAsPromise, correctSentenceAsPromise, scrambledSentenceAsPromise, nextSentenceAsPromise } from '../stores.js';
+import {  correctSentenceAsPromise, nextSentenceAsPromise } from '../stores.js';
 import { API } from './api.js';
 
-export default function init() {
-  if((localStorage.getItem('originalSentence') !== 'undefined') && (parseInt(localStorage.getItem('appScore')) !== 10)) {
-    for (const [key, value] of Object.entries(localStorage)) {
-      if(key === 'originalSentence' && value !=='undefined') {
-        correctSentenceAsPromise.set(value.split(','));
-      }
-      if(key === 'scrambledSentence' && value !=='undefined') {
-        scrambledSentenceAsPromise.set(value);
-      }
-      if(key === 'appScore' && value !=='undefined') {
-        scoreAsPromise.set(value);
-      }
-      if(key === 'nextSentence' && value !== 'undefined') {
-        nextSentenceAsPromise.set(value)
-      }
-    }
+export default async function init(){
+  let sentences = [];
+  const index = parseInt(localStorage.getItem('nextSentence'));
+
+  if (index && index !== 10) {
+    nextSentenceAsPromise.set(index);
   }
   else {
-    if(parseInt(localStorage.getItem('appScore')) === 10) {
-      localStorage.removeItem('originalSentence');
-      localStorage.removeItem('scrambledSentence');
-    }
-    scoreAsPromise.set('0');
-    nextSentenceAsPromise.set('1');
-    API.get(1);
+    nextSentenceAsPromise.set(0);
   }
+
+  await API.getAll().then(res => sentences = res);
+  return sentences;
 
 }
